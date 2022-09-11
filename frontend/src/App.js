b30943f0154eab8f47921369cb692d4788d1377f
Jason-5-37 <2207324024@qq.com>
2022-09-11 20:52:45 +1200

    set up route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Header from './components/header';
+import About from './page/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <>
+      <Header/>
+      <Router>
+      <Switch>
+        <Route exact path="/" component={About} />
+      </Switch>
+      </Router>
+      </>
+    )
+  }
 }
 
 export default App;
