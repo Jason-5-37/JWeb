e09165a58196b25f0352f460299a7190cb5b27d1
Jason-5-37 <2207324024@qq.com>
2022-09-18 22:01:04 +1200

    update css & content

--- src/page/About/index.js
+++ src/page/About/index.js
@@ -5,26 +5,68 @@ class About extends Component {
     render() {
         return (
             <>
-            <h1 id="Title">About</h1>
+            <h1 id="Title">About Me</h1>
 
-            <div id="ContentBox" data-aos="fade-right">
-                <h2 id = "SubTitle">Profile</h2>
+            <div id="ContentBox" className="leftBox" data-aos="fade-right">
+                <h2 id = "SubTitle" className="LeftSubTitle">Profile</h2>
+                <p id = "content">
+                    I am a 2022 AUT graduate with a major in software development and I would like to 
+                    gain a career as a web developer. In my last year of university, I took part in the 
+                    Blendz project and developed an e-commerce website for Blendz company as a 
+                    Scrum Master and developer. From this project, I started to learn about what web 
+                    developers do. Building up the web application makes me feel satisfied and a sense of 
+                    accomplishment.
+                </p>
             </div>
             
-            <div id="ContentBox" data-aos="fade-left">
-                <h2 id = "SubTitle">Skill</h2>
+            <div id="ContentBox" className="rightBox" data-aos="fade-left">
+                <h2 id = "SubTitle" className="RightSubTitle">Skill</h2>
+                <p id = "content">
+                    <li>Full stack development</li>
+                    <li>Project management</li>
+                    <li>UI design</li>
+                    <li>Self-learning</li>
+                    <p>
+                        Technical Skills:
+                        JavaScript, HTML, CSS, PHP, C++, Java, SQL, Firebase, MongoDB, React.js, 
+                        Express.js, Scrum, Heroku, Git, Web development, Project management
+                    </p>
+                    <p>
+                        Languages: 
+                        English, Cantonese, Mandarin
+                    </p>
+                </p>
             </div>
 
-            <div id="ContentBox" data-aos="fade-right">
-                <h2 id = "SubTitle">Project Experience</h2>
+            <div id="ContentBox" className="leftBox" data-aos="fade-right">
+                <h2 id = "SubTitle" className="LeftSubTitle">Project Experience</h2>
+                <p id = "content">
+                <div>Blendz (Commercial development)</div>
+                <div>SPEED (School project)</div>
+                </p>
+
             </div>
 
-            <div id="ContentBox" data-aos="fade-left">
-                <h2 id = "SubTitle">Work Experience</h2>
+            <div id="ContentBox" className="rightBox" data-aos="fade-left">
+                <h2 id = "SubTitle" className="RightSubTitle">Work Experience</h2>
+                <p id = "content">
+                    Super Liquor Onehunga<br/>
+                    Role: Salesperson (Part-time)<br/>
+                    Date: Mar 2019 – Dec 2019<br/><br/>
+                    <li>Provided excellent customer services</li>
+                    <li>Developed sales skills and demonstrated good communication</li>
+                    <li>Time management skills developed by studying while working</li>
+                
+                </p>
+
             </div>
 
-            <div id="ContentBox" data-aos="fade-right">
-                <h2 id = "SubTitle">Interests</h2>
+            <div id="ContentBox" className="leftBox" data-aos="fade-right">
+                <h2 id = "SubTitle" className="LeftSubTitle">Interests</h2>
+                <p id = "content">
+                    <li>Camping</li>
+                    <li>Reading</li>
+                </p>
             </div>
             </>
             )
