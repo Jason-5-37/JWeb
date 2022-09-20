04bab98d5a9e4a294a70b1c3d3d39ea5cc1f7277
Jason-5-37 <2207324024@qq.com>
2022-09-21 11:31:32 +1200

    set up 404

--- src/page/PageNotFound/index.js
+++ src/page/PageNotFound/index.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import "./PageNotFound.css";
+import { Link } from 'react-router-dom';
 
 class PageNotFound extends Component {
     render() {
         return (
-            <div>404</div>
+            <>
+            <div id = "PageNotFoundWrapper">
+            <h1 id="PageNotFoundTitle">404 <br/> Something is not right...</h1>
+            <h3 id="Para">
+            You are trying to open does not exist. You may have mistyped the address, or the
+            page has been moved to another URL. If you think this is an error contact support.    
+            </h3> 
+            
+            <Button type="primary"><Link to={`/`}>Go back to home page</Link></Button>
+            </div>
+            </>
+            
             )
         }
 }
