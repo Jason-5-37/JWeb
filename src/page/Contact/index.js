31832199f7dded80d0e351bec01372dddcf211ba
Jason-5-37 <2207324024@qq.com>
2022-09-22 20:36:21 +1200

    update content

--- src/page/Contact/index.js
+++ src/page/Contact/index.js
@@ -3,7 +3,14 @@ import React, { Component } from "react";
 class Contact extends Component {
     render() {
         return (
-            <div id="frame">sss
+            <div id="frame">
+                <h1>Contact</h1>
+                <h2>Email: zhanhonglu@foxmail.com</h2>
+                <h2>phone: +64 0220326119</h2>
+                <h2>
+                    <a href="https://github.com/Jason-5-37" target="github">GITHUB</a> &nbsp;&nbsp;
+                    <a href="https://www.linkedin.com/in/zhanhong-lu-a068b8227/" target="Linkedin">LINKEDIN</a>
+                </h2>
             </div>
             )
         }
